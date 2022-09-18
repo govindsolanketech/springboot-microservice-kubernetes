097ae19d6d00b5c0ef4041779f032042f1b87a38
govindsolanketech <govind.solanke@outlook.com>
2022-09-19 01:36:27 +0530

    initial commit

---
+++ cloud-config-server/.mvn/wrapper/module-info.java
@@ -0,0 +1,17 @@
+module demo {
+	exports com.gscode.department.service;
+	exports com.gscode.user.service;
+	exports com.gscode.user.repository;
+	exports com.gscode.cloud.gateway;
+	exports com.gscode.user.VO;
+	exports com.gscode.department;
+	exports com.gscode.department.entity;
+	exports com.gscode.department.repository;
+	exports com.gscode.hystrix.dashboard;
+	exports com.gscode.cloud;
+	exports com.gscode.department.controller;
+	exports com.gscode.user.controller;
+	exports com.gscode.service.registry;
+	exports com.gscode.user;
+	exports com.gscode.user.entity;
+}
---
+++ service-registry/src/test/java/com/gscode/service/registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,9 @@
+package com.gscode.service.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+}
